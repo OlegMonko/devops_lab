bf002c2a9376085d751fbc8797e1e4bd30d6f673
Oleg Monko <oleg_monko@epam.com>
2018-07-09 16:18:16 +0300

    Exception handling

--- homework1.sh
+++ homework1.sh
@@ -3,7 +3,7 @@
 pyenv install 2.7.15
 pyenv install 3.5.5
 
-pyenv virtualenv 2.7.15 python2
-pyenv virtualenv 3.5.5 python3
+pyenv virtualenv 2.7.15 python2 || echo "Virtualenv python2 already exists" 
+pyenv virtualenv 3.5.5 python3 || echo "Virtualenv python3 already exists" 
 
 

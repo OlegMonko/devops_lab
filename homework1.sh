035cd9b9c174da8a889902bfa2e09617ab1f1d99
Oleg Monko <oleg_monko@epam.com>
2018-07-11 17:58:22 +0300

    homework3

--- homework1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pyenv install 2.7.15
-pyenv install 3.5.5
-
-pyenv virtualenv 2.7.15 python2 || echo "Virtualenv python2 already exists" 
-pyenv virtualenv 3.5.5 python3 || echo "Virtualenv python3 already exists" 
-
-
--- task5.py
+++
@@ -1,8 +0,0 @@
-keyb = 'qwertyuiopasdfghjklzxcvbnm'
-f = open('INPUT.TXT', 'r')
-i = keyb.find(f.read(1))
-f.close()
-
-f = open('OUTPUT.TXT', 'w')
-f.write(keyb[i + 1])
-f.close()

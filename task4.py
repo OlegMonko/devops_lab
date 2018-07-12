48b867112c43c5a2e4f70c8d4deab34a13fec1f3
Oleg Monko <oleg_monko@epam.com>
2018-07-12 12:05:36 +0300

    made changes requested

---
+++ task4.py
@@ -0,0 +1,13 @@
+import collections
+
+print('Enter the number of words')
+n = int(input())
+
+c = collections.Counter()
+for i in range(n):
+    print('Enter', i + 1, 'word')
+    c[input()] += 1
+
+print(len(c))
+for i in c.values():
+    print(i, end=' ')
---
+++ task5.py
@@ -0,0 +1,8 @@
+keyb = 'qwertyuiopasdfghjklzxcvbnm'
+f = open('INPUT.TXT', 'r')
+i = keyb.find(f.read(1))
+f.close()
+
+f = open('OUTPUT.TXT', 'w')
+f.write(keyb[i + 1])
+f.close()
